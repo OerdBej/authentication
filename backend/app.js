4a4ece0ea87c64b15859768de94d5ca3c63520f4
oerdbej <oerdbej@gmail.com>
2025-04-11 15:32:55 +0200

    user schema from mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './database/db.js';
+import authRoutes from './routes/auth.js';
 
 dotenv.config();
 
@@ -9,6 +10,9 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('hello this world');
 });
+
+//shortcut
+app.use('/api/auth', authRoutes);
 
 app.listen(3000, () => {
   connectDB();
