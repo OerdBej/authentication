2e5ac2d82a932e419ae4940a36ae367117f3f805
oerdbej <oerdbej@gmail.com>
2025-04-13 18:33:11 +0200

    hashed password for new user

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,4 +1,5 @@
 import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
 
 export const signup = async (req, res, next) => {
   const { email, password, name } = req.body;
@@ -14,8 +15,17 @@ export const signup = async (req, res, next) => {
     if (userExist) {
       throw new Error('User already exists');
     }
+
+    //hash the password and create the new user
+    const hashPassword = await bcrypt.hash(password, 10);
+    const verificationToken = generateVerificationToken();
+    const user = await User.create({
+      email,
+      password: hashPassword,
+      name,
+    });
   } catch (error) {
-    next(error);
+    res.status(400).json({ success: false, message: error.message });
   }
 };
 
