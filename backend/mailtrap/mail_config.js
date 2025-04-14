3be8a47bc51bfee9c927034298b65bad5e0b78eb
oerdbej <oerdbej@gmail.com>
2025-04-14 23:59:24 +0200

    mailtrap configuration template from google  = check verification token

--- backend/mailtrap/mail_config.js
+++ backend/mailtrap/mail_config.js
@@ -5,28 +5,12 @@ dotenv.config();
 
 const TOKEN = process.env.MAILTRAP_TOKEN;
 
-const client = new MailtrapClient({
+//recipients should be dynamic later from the user that is signed up. TESTING
+export const mailclient = new MailtrapClient({
   token: TOKEN,
 });
 
-const sender = {
+export const sender = {
   email: 'hello@demomailtrap.co',
   name: 'Oerd Bej ',
 };
-
-//recipients should be dynamic later from the user that is signed up. TESTING
-const recipients = [
-  {
-    email: 'opencode03@gmail.com',
-  },
-];
-
-client
-  .send({
-    from: sender,
-    to: recipients,
-    subject: 'You are awesome!',
-    text: 'Congrats for sending test email with Mailtrap!',
-    category: 'Integration Test',
-  })
-  .then(console.log, console.error);
