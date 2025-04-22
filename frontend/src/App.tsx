8942959a8bdf76333f298af304c38e773c5f2c28
oerdbej <oerdbej@gmail.com>
2025-04-22 13:09:28 +0200

    starting typescript react with tailwind 4.0

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h2 className='text-2xl text-white bg-black p-2'>
+        the most sophisticated front 🤪
+      </h2>
+    </>
+  );
+}
+
+export default App;
